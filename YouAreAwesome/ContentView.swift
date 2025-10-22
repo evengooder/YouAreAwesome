7c07f29b93db1f61f0140d8e82d65c4009866d67
Justin Johnson <evengooder@gmail.com>
2025-10-21 18:10:29 -0700

    mo mo

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,19 +12,37 @@ struct ContentView: View {
     @State var message = "I am a programer!!!"
     var body: some View {
         
+        
+        
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
             
-            Button("Click me!") {
-                message = "Awesome!"
+            Spacer()
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
             }
+           
             
         }
         .padding()
