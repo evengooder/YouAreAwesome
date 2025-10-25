ee14f7e80aeea6fb6604a1bd9ff21f5338419819
Justin Johnson <evengooder@gmail.com>
2025-10-25 12:32:56 -0700

    backup

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,38 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var message = "I am a programer!!!"
+    @State var message = ""
+    @State var image4me = ""
+    
     var body: some View {
-        
+    
         
         
         VStack {
+            
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.purple)
+            
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: image4me)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .foregroundStyle(.black)
                 .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .foregroundStyle(.purple)
+            
             
             Spacer()
             HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
+                Button("Peace") {
+                    message = "Peace"
+                    image4me = "peacesign"
+                    
                 }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-                .tint(.orange)
                 
-                Button("Great!") {
-                    message = "Great!"
+                
+                
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
+                
+                Button("Love") {
+                    message = "Love"
+                    image4me = "heart"
+                 
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
-                .tint(.orange)
+                .tint(.purple)
+                
+                Button("Understanding") {
+                    message = "Great!"
+                    image4me = "lightbulb"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.purple)
             }
            
             
